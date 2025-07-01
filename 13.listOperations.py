595ac7f9184f52cc5675105a40e6ca555218bd36
ansh bathla <bathlaansh382@gmail.com>
2025-07-02 00:53:09 +0530

    list operatinos

--- 13.listOperations.py
+++ 13.listOperations.py
@@ -8,3 +8,28 @@ for i in range(len(list)):
 empty=[]
 for i in empty:
     print("empty list") #--> So it does not show error but also it doesn't print anything
+
+
+#insert and update the list
+Mylist=[2,3,4,2,4,5,3]
+Mylist.insert(4,34) #--> we can add elements to the any index with insert method
+print(Mylist)
+Mylist.append(34) #--> append method is used to add element at the end of the list
+print(Mylist)
+extendedList=[33,33,44,5,55,99]
+Mylist.extend(extendedList)
+print(Mylist)
+
+#update list through slice method
+Mylist1=['a','b','c','d','e']
+Mylist1[0:3]=['x','y','z']
+print(Mylist1)
+
+#deletion operations in the list
+
+Mylist1.pop(2)
+print(Mylist1)
+del Mylist1[0:2]
+print(Mylist1)
+Mylist1.remove('e')
+print(Mylist1)
